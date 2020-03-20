eaeed952f67a37a8b86cbcd144475d7228ac5d56
Peter Sandqvist <petersandqvist@Peters-MacBook-Pro.local>
2020-03-20 15:47:50 +0100

    button now work with finding id of location

--- Locations/components/Button.js
+++ Locations/components/Button.js
@@ -1,17 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, FlatList, Button} from 'react-native';
+import axios from 'axios';
 
-const FollowButton = () => {
+const FollowButton = props => {
   const [following, setFollowing] = useState(false);
   return (
     <Button
-      onPress={() =>
-        following === true ? setFollowing(false) : setFollowing(true)
-      }
+      onPress={() => (
+        props.onFollow(props.id, !following), setFollowing(!following)
+      )}
       title={following ? 'following' : 'follow'}
       color={following ? 'green' : '#000'}
     />
   );
 };
-
 export default FollowButton;
--- Locations/components/GetLocations.js
+++ Locations/components/GetLocations.js
@@ -1,39 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, FlatList, Button} from 'react-native';
+import {getDistance} from 'geolib';
 import axios from 'axios';
 import FollowButton from './Button';
 
 const GetLocations = () => {
   const [locations, setLocations] = useState([]);
-
   useEffect(() => {
     axios
       .get(
         'https://storage.googleapis.com/engineering-4b0b7d62/locations_filtered.json',
       )
       .then(response => {
-        console.log(response.data);
         return setLocations(response.data);
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
+  const updateFollowing = (id, follow) => {
+    const newLocations = locations.slice(0);
+    if (!newLocations) {
+      newLocations.find(l => l.id === id).following = follow;
+      setLocations(newLocations);
+    }
+  };
 
   return (
     <View>
       <FlatList
         data={locations}
-        keyExtractor={item => item.id.toString()} //item.id.toString()
+        keyExtractor={item => item.id.toString()}
         renderItem={({item}) => (
-          //console.log(item.name);
           <View style={styles.row}>
             <Text style={styles.text}>{item.name}</Text>
             <Text>
               {item.latitude} {item.longitude}
             </Text>
             <View style={styles.buttonContainer}>
-              <FollowButton />
+              <FollowButton locId={item.id} onFollow={updateFollowing} />
             </View>
           </View>
         )}
